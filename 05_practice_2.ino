e2a6cc65db47f57b8df9b5534e5bbedc15ce5c90
khs1115 <asd7480asd@gmail.com>
2025-09-23 00:09:05 +0900

    Add files via upload

---
+++ 05_practice_2.ino
@@ -0,0 +1,28 @@
+#define PIN_LED 7
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN_LED,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(PIN_LED, 0);  
+  delay(1000);                 
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(PIN_LED, 1);  // LED OFF
+    delay(100);                  // 0.1초
+    digitalWrite(PIN_LED, 0); // LED ON
+    delay(100);
+
+    
+  }
+  digitalWrite(PIN_LED, 1);
+  
+  while (1) {
+    
+    }
+
+
+}
